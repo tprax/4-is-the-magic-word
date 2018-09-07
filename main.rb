385fc4cfeea6bdbe0a5c2693e54f8c016721a9dc
tprax <tprax01@gmail.com>
2018-09-07 10:26:09 -0600

    initail commit

---
+++ main.rb
@@ -0,0 +1,27 @@
+#get a number
+#change that number into a word
+#count the number of letters in that word
+#output the number of letters count
+#count till you git a word to four
+#four is the magic number
+require "to_words"
+
+@word = []
+
+def number
+  puts "Welcome to 4 is the magic number!"
+  puts "Please enter a number!"
+  @user_number = gets.strip.to_i
+end
+number
+
+puts @user_number.to_words
+
+def number1
+  @word << @magic.split('').count
+end
+
+def magic4(number1)
+  
+end
+
